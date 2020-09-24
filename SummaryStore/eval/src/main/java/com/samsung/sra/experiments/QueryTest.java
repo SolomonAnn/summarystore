79dcfe3426d951b0bff123f2255122bb1cd85769
SolomonAnn <thss15_anyz@163.com>
2020-09-24 20:52:12 +0800

    modify query test

--- SummaryStore/eval/src/main/java/com/samsung/sra/experiments/QueryTest.java
+++ SummaryStore/eval/src/main/java/com/samsung/sra/experiments/QueryTest.java
@@ -19,14 +19,10 @@ import com.samsung.sra.datastore.ResultError;
 import com.samsung.sra.datastore.StreamException;
 import com.samsung.sra.datastore.SummaryStore;
 import com.samsung.sra.datastore.storage.BackingStoreException;
-import javafx.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Random;
 
 public class QueryTest {
@@ -38,12 +34,22 @@ public class QueryTest {
                                      31_250_000_000L, 31_250_000_000L, 31_250_000_000L,
                                      31_250_000_000L};
 
-    private static final int TOTAL_STREAM_NUM = 10;
-    private static final int QUERY_TIME = 5;
+    private static int TOTAL_STREAM_NUM = 10;
+    private static int QUERY_TIME = 5;
+    private static int MONTH_OFFSET_LEN = 2;
 
     private static final String directory = "/data/tdstore_throughput";
 
     public static void main(String[] args) throws BackingStoreException, IOException, ClassNotFoundException {
+        if(args.length < 3){
+            System.err.println("SYNTAX: TOTAL_STREAM_NUM QUERY_NUM MONTH_OFFSET_LEN ");
+            System.exit(2);
+        }
+
+        TOTAL_STREAM_NUM = Integer.parseInt(args[0]);
+        QUERY_TIME = Integer.parseInt(args[1]);
+        MONTH_OFFSET_LEN = Integer.parseInt(args[2]);
+
         SummaryStore store = new SummaryStore(directory);
         long st = System.currentTimeMillis();
         QueryTest queryTest = new QueryTest();
@@ -53,31 +59,7 @@ public class QueryTest {
         queryTest.queryTest(store, 0, QUERY_TIME);
         queryTest.queryTest(store, 1, QUERY_TIME);
 
-        logger.info("ALL TASK FINISH in {} min", (System.currentTimeMillis() - st) / 1000 / 60.0);
-    }
-
-    public Pair<List<Long>, List<Double>> timedQuery(SummaryStore store, long intervalTimeInMs, int queryNum, int aggreFun, long startTime, TimeUnit queryLen){
-        List<Long> latencys = new LinkedList<>();
-        List<Double> result = new ArrayList<>();
-        for (int i = 0; i < queryNum; i++) {
-            long t0 = System.currentTimeMillis();
-            double res = 0;
-            try {
-                ResultError re = (ResultError) store.query(0L, startTime,startTime + queryLen.timeInMs, aggreFun);
-                res = Double.parseDouble(re.result.toString());
-            } catch (StreamException | BackingStoreException e) {
-                logger.info(e.getMessage());
-            }
-            result.add(res);
-            try {
-                Thread.sleep(intervalTimeInMs);
-            } catch (InterruptedException e) {
-                logger.info(e.getMessage());
-            }
-            long t1 = System.currentTimeMillis();
-            latencys.add((t1 - t0));
-        }
-        return new Pair(latencys, result);
+        logger.info("-QUERY-ALL TASK FINISH in {} min", (System.currentTimeMillis()-st)/1000/60.0);
     }
 
     public void queryTest(SummaryStore store, int aggreFun, int totalTimes) {
@@ -87,10 +69,10 @@ public class QueryTest {
         long[][][] latency = new long [totalTimes][4][4];
         double[][][] result = new double [totalTimes][4][4];
 
-        for(int i = 0; i < totalTimes; i++){
+        for(int i = 0; i< totalTimes; i++){
             for(TimeUnit offset: TimeUnit.values()){
                 long streamID = random.nextInt(TOTAL_STREAM_NUM);
-                long endTime = N[(int)streamID] - offset.timeInMs;
+                long endTime = N[(int)streamID] - queryOffsetLen(offset) * offset.timeInMs;
                 int range = random.nextInt((int) (offset.timeInMs * 0.05));
                 endTime += random.nextDouble() > 0.5 ? range: -range;
                 for(TimeUnit queryLen : TimeUnit.values()){
@@ -113,13 +95,25 @@ public class QueryTest {
         printQueryResult(result);
     }
 
-    private void printLatency(long[][][] latency) {
+    private int queryOffsetLen(TimeUnit offset){
+        if(offset.equals(TimeUnit.MINUTE)){
+            return random.nextInt(60);
+        } else if(offset.equals(TimeUnit.HOUR)){
+            return random.nextInt(24);
+        } else if(offset.equals(TimeUnit.DAY)){
+            return random.nextInt(30);
+        } else {
+            return MONTH_OFFSET_LEN;
+        }
+    }
+
+    private void printLatency(long[][][] latency){
         int totalTimes = latency.length;
-        logger.info("****Latency in ms(row:offset, col:queryLen)***");
-        for(int i = 0; i < totalTimes; i++) {
-            logger.info("Time {}:",i);
-            for(TimeUnit offset: TimeUnit.values()) {
-                logger.info("{},{},{},{},",
+        logger.info("-QUERY-****Latency in ms(row:offset, col:queryLen)***");
+        for(int i = 0; i < totalTimes; i++){
+            logger.info("-QUERY-Time {}:",i);
+            for(TimeUnit offset: TimeUnit.values()){
+                logger.info("-QUERY-,{},{},{},{},",
                     latency[i][offset.ordinal()][0],
                     latency[i][offset.ordinal()][1],
                     latency[i][offset.ordinal()][2],
@@ -128,7 +122,7 @@ public class QueryTest {
             }
         }
 
-        logger.info("Latency ---- Avg:");
+        logger.info("-QUERY-Latency ---- Avg:");
         for(TimeUnit offset: TimeUnit.values()){
             long minuteLenRes = 0;
             long hourLenRes = 0;
@@ -140,21 +134,22 @@ public class QueryTest {
                 dayLenRes += longs[offset.ordinal()][2];
                 monthLenRes += longs[offset.ordinal()][3];
             }
-            logger.info("{},{},{},{},",
-                minuteLenRes/totalTimes,
-                hourLenRes/totalTimes,
-                dayLenRes/totalTimes,
-                monthLenRes/totalTimes);
+            logger.info("-QUERY-,{},{},{},{},",
+                minuteLenRes / totalTimes,
+                hourLenRes / totalTimes,
+                dayLenRes / totalTimes,
+                monthLenRes / totalTimes
+            );
         }
     }
 
     private void printQueryResult(double[][][] result){
         int totalTimes = result.length;
-        logger.info("****Query Result(row:offset, col:queryLen)***");
+        logger.info("-QUERY-****Query Result(row:offset, col:queryLen)***");
         for(int i = 0; i < totalTimes; i++){
-            logger.info("Time {}:", i);
+            logger.info("-QUERY-Time {}:", i);
             for(TimeUnit offset: TimeUnit.values()){
-                logger.info("{},{},{},{},",
+                logger.info("-QUERY-,{},{},{},{},",
                     result[i][offset.ordinal()][0],
                     result[i][offset.ordinal()][1],
                     result[i][offset.ordinal()][2],
@@ -163,7 +158,7 @@ public class QueryTest {
             }
         }
 
-        logger.info("Query Result --- Avg:");
+        logger.info("-QUERY-Query Result --- Avg:");
         for(TimeUnit offset: TimeUnit.values()){
             double minuteLenRes = 0;
             double hourLenRes = 0;
@@ -175,19 +170,19 @@ public class QueryTest {
                 dayLenRes += doubles[offset.ordinal()][2];
                 monthLenRes += doubles[offset.ordinal()][3];
             }
-            logger.info("{},{},{},{},",
-                minuteLenRes/totalTimes,
-                hourLenRes/totalTimes,
-                dayLenRes/totalTimes,
-                monthLenRes/totalTimes
+            logger.info("-QUERY-,{},{},{},{},",
+                minuteLenRes / totalTimes,
+                hourLenRes / totalTimes,
+                dayLenRes / totalTimes,
+                monthLenRes / totalTimes
             );
         }
     }
 
     enum TimeUnit{
-        MINUTE(60_000L,"MIN"),
-        HOUR(3_600_000L,"HOUR"),
-        DAY(86_400_000L,"DAY"),
+        MINUTE(60_000L, "MIN"),
+        HOUR(3_600_000L, "HOUR"),
+        DAY(86_400_000L, "DAY"),
         MONTH(2_592_000_000L, "MON");
 
         long timeInMs;
