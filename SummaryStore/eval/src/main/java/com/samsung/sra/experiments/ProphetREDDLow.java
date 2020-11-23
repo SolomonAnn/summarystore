4692a4bf5747f875cccb07f0e54ced4e2f2bf910
SolomonAnn <thss15_anyz@163.com>
2020-11-23 18:31:29 +0800

    modify prophet redd low

--- SummaryStore/eval/src/main/java/com/samsung/sra/experiments/ProphetREDDLow.java
+++ SummaryStore/eval/src/main/java/com/samsung/sra/experiments/ProphetREDDLow.java
@@ -12,26 +12,17 @@ import java.io.IOException;
 public class ProphetREDDLow {
 	private static final Logger logger = LoggerFactory.getLogger(ProphetREDDLow.class);
 
-	private static final long start[] = {1303132929L, 1303082307L, 1302930687L, 1303002979L, 1303100643L};
-	private static final long end[] = {158010967022L, 303951108756L, 194832201194L, 208575498345L, 948051387620L};
+	private static final long end[] = {158_010_967_022L};
 
-	private static int[] stream = {0, 1, 2, 3, 4};
+	private static int[] stream = {0};
 	private static long[][] offset = {
-		{1000 * TimeUnit.YEAR.timeInSec, 2000 * TimeUnit.YEAR.timeInSec, 0},
-		{1000 * TimeUnit.YEAR.timeInSec, 2000 * TimeUnit.YEAR.timeInSec, 3000 * TimeUnit.YEAR.timeInSec, 0},
-		{1000 * TimeUnit.YEAR.timeInSec, 2000 * TimeUnit.YEAR.timeInSec, 0},
-		{1000 * TimeUnit.YEAR.timeInSec, 2000 * TimeUnit.YEAR.timeInSec, 0},
-		{1000 * TimeUnit.YEAR.timeInSec, 2000 * TimeUnit.YEAR.timeInSec, 0}
+		{0, 4000 * TimeUnit.YEAR.timeInSec},
 	};
 	private static long[] range = {
-		370 * TimeUnit.DAY.timeInSec,
-		350 * TimeUnit.DAY.timeInSec,
-		450 * TimeUnit.DAY.timeInSec,
-		480 * TimeUnit.DAY.timeInSec,
-		440 * TimeUnit.DAY.timeInSec
+		37 * TimeUnit.DAY.timeInSec,
 	};
 
-	private static int QUERY_TIME = 10000;
+	private static int QUERY_TIME = 1000;
 
 	private static final String directory = "/data/tdstore_throughput_redd_low";
 
@@ -44,29 +35,21 @@ public class ProphetREDDLow {
 		QUERY_TIME = Integer.parseInt(args[0]);
 
 		SummaryStore store = new SummaryStore(directory);
-		long st = System.currentTimeMillis();
 		ProphetREDDLow prophetREDDLow = new ProphetREDDLow();
 
-		for (int i = 0; i < stream.length; i++) {
-			logger.info("streamID {}", stream[i]);
-			prophetREDDLow.queryTest(store, stream[i]);
+		for (int j : stream) {
+			prophetREDDLow.queryTest(store, j);
 		}
-
-		logger.info("-QUERY-ALL TASK FINISH in {} min", (System.currentTimeMillis() - st) / 1000 / 60.0);
 	}
 
 	public void queryTest(SummaryStore store, int streamID) {
-		long st = System.currentTimeMillis();
-
-		long[][] latency = new long [offset[streamID].length][QUERY_TIME];
+		long[][] timestamp = new long [offset[streamID].length][QUERY_TIME];
 		double[][] result = new double [offset[streamID].length][QUERY_TIME];
 
 		for (int i = 0; i < offset[streamID].length; i++) {
 			long endTime = end[streamID] - offset[streamID][i];
 			long startTime = endTime - range[streamID];
 			for (int j = 0; j < QUERY_TIME; j++) {
-				logger.info("stream = {}, startTime = {}, endTime = {}", streamID, startTime, endTime);
-				long t0 = System.currentTimeMillis();
 				try {
 					ResultError sum = (ResultError) store.query(streamID, startTime + j * range[streamID] / QUERY_TIME, startTime + (j + 1) * range[streamID] / QUERY_TIME, 3);
 					ResultError count = (ResultError) store.query(streamID, startTime + j * range[streamID] / QUERY_TIME, startTime + (j + 1) * range[streamID] / QUERY_TIME, 2);
@@ -74,32 +57,16 @@ public class ProphetREDDLow {
 				} catch (StreamException | BackingStoreException e) {
 					logger.info(e.getMessage());
 				}
-				long t1 = System.currentTimeMillis();
-				latency[i][j] = t1 - t0;
+				timestamp[i][j] = (startTime + j * range[streamID] / QUERY_TIME + startTime + (j + 1) * range[streamID] / QUERY_TIME) / 2;
 			}
 		}
-
-		logger.info("-QUERY-Execute {} queries in {} s.", stream.length * stream.length, (System.currentTimeMillis() - st) / 1000);
-		printLatency(latency, streamID);
-		printQueryResult(result, streamID);
+		print(timestamp, result, streamID);
 	}
 
-	private void printLatency(long[][] latency, int streamID) {
-		logger.info("----------Latency----------");
+	private void print(long[][] timestamp, double[][] result, int streamID){
 		for (int i = 0; i < offset[streamID].length; i++) {
-			logger.info("StreamID {}:", stream[i]);
 			for (int j = 0; j < QUERY_TIME; j++) {
-				logger.info("Time {} {}", j, latency[i][j]);
-			}
-		}
-	}
-
-	private void printQueryResult(double[][] result, int streamID){
-		logger.info("----------Result----------");
-		for (int i = 0; i < offset[streamID].length; i++) {
-			logger.info("StreamID {}:", stream[i]);
-			for (int j = 0; j < QUERY_TIME; j++) {
-				logger.info("Time {} {}", j, result[i][j]);
+				logger.info("{},{}", timestamp[i][j], result[i][j]);
 			}
 		}
 	}
